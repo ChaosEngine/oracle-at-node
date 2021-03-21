ca31c7b8acd5b9b2b56dab8173b0d4911b0db4ea
Chaos Engine <andrzej.pauli@gmail.com>
2021-03-21 21:58:24 +0100

    formatting

--- index.js
+++ index.js
@@ -45,102 +45,102 @@ const dbConfig = require('./dbconfig.js');
 // oracledb.initOracleClient({ libDir: '/Users/your_username/Downloads/instantclient_19_8' }); // macOS
 
 async function run() {
-  let connection;
+	let connection;
 
-  try {
+	try {
 
-    let sql, binds, options, result;
+		let sql, binds, options, result;
 
-    connection = await oracledb.getConnection(dbConfig);
+		connection = await oracledb.getConnection(dbConfig);
 
-    //
-    // Create a table
-    //
+		//
+		// Create a table
+		//
 
-    const stmts = [
-      `DROP TABLE no_example`,
+		const stmts = [
+			`DROP TABLE no_example`,
 
-      `CREATE TABLE no_example (id NUMBER, data VARCHAR2(20))`
-    ];
+			`CREATE TABLE no_example (id NUMBER, data VARCHAR2(20))`
+		];
 
-    for (const s of stmts) {
-      try {
-        await connection.execute(s);
-      } catch(e) {
-        if (e.errorNum != 942)
-          console.error(e);
-      }
-    }
+		for (const s of stmts) {
+			try {
+				await connection.execute(s);
+			} catch (e) {
+				if (e.errorNum != 942)
+					console.error(e);
+			}
+		}
 
-    //
-    // Insert three rows
-    //
+		//
+		// Insert three rows
+		//
 
-    sql = `INSERT INTO no_example VALUES (:1, :2)`;
+		sql = `INSERT INTO no_example VALUES (:1, :2)`;
 
-    binds = [
-      [101, "Alpha" ],
-      [102, "Beta" ],
-      [103, "Gamma" ]
-    ];
+		binds = [
+			[101, "Alpha"],
+			[102, "Beta"],
+			[103, "Gamma"]
+		];
 
-    // For a complete list of options see the documentation.
-    options = {
-      autoCommit: true,
-      // batchErrors: true,  // continue processing even if there are data errors
-      bindDefs: [
-        { type: oracledb.NUMBER },
-        { type: oracledb.STRING, maxSize: 20 }
-      ]
-    };
+		// For a complete list of options see the documentation.
+		options = {
+			autoCommit: true,
+			// batchErrors: true,  // continue processing even if there are data errors
+			bindDefs: [
+				{ type: oracledb.NUMBER },
+				{ type: oracledb.STRING, maxSize: 20 }
+			]
+		};
 
-    result = await connection.executeMany(sql, binds, options);
+		result = await connection.executeMany(sql, binds, options);
 
-    console.log("Number of rows inserted:", result.rowsAffected);
+		console.log("Number of rows inserted:", result.rowsAffected);
 
-    //
-    // Query the data
-    //
+		//
+		// Query the data
+		//
 
-    sql = `SELECT * FROM no_example`;
+		sql = `SELECT * FROM no_example`;
 
-    binds = {};
+		binds = {};
 
-    // For a complete list of options see the documentation.
-    options = {
-      outFormat: oracledb.OUT_FORMAT_OBJECT,   // query result format
-      // extendedMetaData: true,               // get extra metadata
-      // prefetchRows:     100,                // internal buffer allocation size for tuning
-      // fetchArraySize:   100                 // internal buffer allocation size for tuning
-    };
+		// For a complete list of options see the documentation.
+		options = {
+			outFormat: oracledb.OUT_FORMAT_OBJECT,   // query result format
+			// extendedMetaData: true,               // get extra metadata
+			// prefetchRows:     100,                // internal buffer allocation size for tuning
+			// fetchArraySize:   100                 // internal buffer allocation size for tuning
+		};
 
-    result = await connection.execute(sql, binds, options);
+		result = await connection.execute(sql, binds, options);
 
-    console.log("Metadata: ");
-    console.dir(result.metaData, { depth: null });
-    console.log("Query results: ");
-    console.dir(result.rows, { depth: null });
+		console.log("Metadata: ");
+		console.dir(result.metaData, { depth: null });
+		console.log("Query results: ");
+		console.dir(result.rows, { depth: null });
 
-    //
-    // Show the date.  The value of ORA_SDTZ affects the output
-    //
+		//
+		// Show the date.  The value of ORA_SDTZ affects the output
+		//
 
-    sql = `SELECT TO_CHAR(CURRENT_DATE, 'DD-Mon-YYYY HH24:MI') AS CD FROM DUAL`;
-    result = await connection.execute(sql, binds, options);
-    console.log("Current date query results: ");
-    console.log(result.rows[0]['CD']);
+		sql = `SELECT TO_CHAR(CURRENT_DATE, 'DD-Mon-YYYY HH24:MI') AS CD FROM DUAL`;
+		result = await connection.execute(sql, binds, options);
+		console.log("Current date query results: ");
+		console.log(result.rows[0]['CD']);
 
-  } catch (err) {
-    console.error(err);
-  } finally {
-    if (connection) {
-      try {
-        await connection.close();
-      } catch (err) {
-        console.error(err);
-      }
-    }
-  }
+	} catch (err) {
+		console.error(err);
+	} finally {
+		if (connection) {
+			try {
+				await connection.close();
+			} catch (err) {
+				console.error(err);
+			}
+		}
+	}
 }
 
 run();
