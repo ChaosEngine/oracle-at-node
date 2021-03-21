aa29931b4c7571c7a089b2018ea84cfa049ac887
Chaos Engine <andrzej.pauli@gmail.com>
2021-03-21 21:58:10 +0100

    required libs/deps size optimizations

--- prepare.sh
+++ prepare.sh
@@ -7,8 +7,8 @@ apt-get update && apt-get install -y libaio1 wget unzip
 mkdir -p /opt/oracle
 cd /opt/oracle
 wget https://download.oracle.com/otn_software/linux/instantclient/211000/instantclient-basiclite-linux.x64-21.1.0.0.0.zip
-unzip instantclient-basiclite-linux.x64-21.1.0.0.0.zip
-
+unzip instantclient-basiclite-linux.x64-21.1.0.0.0.zip && rm -f instantclient-basiclite-linux.x64-21.1.0.0.0.zip
+cd /opt/oracle/instantclient_21_1 && rm -f *jdbc* *occi* *mysql* *mql1* *ipc1* *jar uidrvci genezi adrci
 
 echo /opt/oracle/instantclient_21_1 > /etc/ld.so.conf.d/oracle-instantclient.conf
 ldconfig
