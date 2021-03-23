7dbe4d43f5a88827ab2291cc7504ed127a621eab
Chaos <andrzej.pauli@gmail.com>
2021-03-23 09:05:56 +0100

    Create README.md

--- dbconfig.js
+++ dbconfig.js
@@ -72,15 +72,15 @@
  *
  *****************************************************************************/
 
- module.exports = {
-	user: process.env.NODE_ORACLEDB_USER || "XXXXXXXXX",
+module.exports = {
+	user: process.env.NODE_ORACLEDB_USER || "USERUSERUSER",
 
 	// Get the password from the environment variable
 	// NODE_ORACLEDB_PASSWORD.  The password could also be a hard coded
 	// string (not recommended), or it could be prompted for.
 	// Alternatively use External Authentication so that no password is
 	// needed.
-	password: process.env.NODE_ORACLEDB_PASSWORD || "YYYYYYYYYYYY",
+	password: process.env.NODE_ORACLEDB_PASSWORD || "PASSPASSPASSPASS",
 
 	// For information on connection strings see:
 	// https://oracle.github.io/node-oracledb/doc/api.html#connectionstrings
